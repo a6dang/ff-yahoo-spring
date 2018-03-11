3ed5cc07b78d18c2a367a76fe7328db34d93a6ec
anthony.dang <anthony.dang@workday.com>
2018-03-10 16:31:22 -0800

    maybe
    Change-Id: I23dcf347ccf08625434453ae1485a00aae46f6f3

--- src/main/java/com/adang/fantasymessenger/DemoApplication.java
+++ src/main/java/com/adang/fantasymessenger/DemoApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
     private static final String LINK = "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9YWxnaVVSTmFTbW90JmQ9WVdrOU5GSlBjRmh1TlRJbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lNw--&redirect_uri=https://gentle-dusk-83365.herokuapp.com/oauth&response_type=code&language=en-us";
-//    private static final String LINK = "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9VEI0ZUxBZkhKeTcyJmQ9WVdrOVkyODVZelpSTnpnbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD04NQ--&redirect_uri=https://127.0.0.1:8080/oauth&response_type=code&language=en-us";
+//    private static final String LINK = "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9am44aTRiemZnc3VnJmQ9WVdrOWNWcFdVV1ZLTldrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1hNA--&redirect_uri=https://localhost:8080/oauth&response_type=code&language=en-us";
 
     @RequestMapping("/")
     @ResponseBody
--- src/main/java/com/adang/fantasymessenger/auth/OAuthController.java
+++ src/main/java/com/adang/fantasymessenger/auth/OAuthController.java
@@ -17,17 +17,14 @@ public class OAuthController {
     private static final String CLIENT_ID = "dj0yJmk9YWxnaVVSTmFTbW90JmQ9WVdrOU5GSlBjRmh1TlRJbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lNw--";
     private static final String CLIENT_SECRET = "d52d279acefcac5e35be7bf9965b82d2a4060cff";
     private static final String AUTHORIZATION_HEADER = "Basic ZGoweUptazlZV3huYVZWU1RtRlRiVzkwSm1ROVdWZHJPVTVHU2xCalJtaDFUbFJKYldOSGJ6bE5RUzB0Sm5NOVkyOXVjM1Z0WlhKelpXTnlaWFFtZUQxbE53LS06ZDUyZDI3OWFjZWZjYWM1ZTM1YmU3YmY5OTY1YjgyZDJhNDA2MGNmZg==";
-    private static final String REDIRECT_URI = "gentle-dusk-83365.herokuapp.com";
     // local
 //    private static final String CLIENT_ID = "dj0yJmk9VEI0ZUxBZkhKeTcyJmQ9WVdrOVkyODVZelpSTnpnbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD04NQ--";
 //    private static final String CLIENT_SECRET = "9ce094a5c54372cfba4024a6237457d150b803f4";
 //    private static final String AUTHORIZATION_HEADER = "Basic ZGoweUptazlWRUkwWlV4QlpraEtlVGN5Sm1ROVdWZHJPVmt5T0RWWmVscFNUbnBuYldOSGJ6bE5RUzB0Sm5NOVkyOXVjM1Z0WlhKelpXTnlaWFFtZUQwNE5RLS06OWNlMDk0YTVjNTQzNzJjZmJhNDAyNGE2MjM3NDU3ZDE1MGI4MDNmNA==";
-//    private static final String REDIRECT_URI = "https://locahost:8080/success";
 
 //    private static final String CLIENT_ID = "dj0yJmk9NFRtSUVzdXpIVWxoJmQ9WVdrOU9WSlVVbTlUTlRJbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD02OQ--";
 //    private static final String CLIENT_SECRET = "1b8579a5bc055f9eb6849cdf850793eb125b9c27";
 //    private static final String AUTHORIZATION_HEADER = "Basic ZGoweUptazlORlJ0U1VWemRYcElWV3hvSm1ROVdWZHJPVTlXU2xWVmJUbFVUbFJKYldOSGJ6bE5RUzB0Sm5NOVkyOXVjM1Z0WlhKelpXTnlaWFFtZUQwMk9RLS06MWI4NTc5YTViYzA1NWY5ZWI2ODQ5Y2RmODUwNzkzZWIxMjViOWMyNw==";
-//    private static final String REDIRECT_URI = "https://localfantasybot.com:8080/success";
 
     @RequestMapping(value = "/oauth", method = RequestMethod.GET)
     String handleOAuthRedirect(@RequestParam("code") String authCode) {
